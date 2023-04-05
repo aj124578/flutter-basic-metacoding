03b5a2808fd6e51c78133b655a26a90b8a61280f
aj124578 <aj124578@naver.com>
2023-04-06 01:16:32 +0900

    dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
@@ -5,116 +6,74 @@ void main() {
   runApp(FirstApp());
 }
 
-// StatelessWidget 위젯은 한번 그려지면 다시 안그려짐.
-// StatefulWidget 데이터 변경시 다시 그림이 그려짐. Wideget build 함수가 재실행 됨
-class FirstApp extends StatefulWidget {
-  FirstApp({Key? key}) : super(key: key);
+class NewApp extends StatelessWidget {
+  const NewApp({Key? key}) : super(key: key);
 
   @override
-  State<FirstApp> createState() => _FirstAppState();
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => buildAlertDialog(context),
+          );
+        },
+        child: Text("버튼"),
+      ),
+    );
+  }
+
+//   CupertinoAlertDialog buildAlertDialog(BuildContext context) {
+//     return CupertinoAlertDialog(
+//       title: Text("다이어로그"),
+//       content: Text("내용입니다..........."),
+//       actions: [
+//         TextButton(onPressed: () {
+//
+//         }, child: Text("적용"))
+//       ],
+//     );
+//   }
 }
 
-class _FirstAppState extends State<FirstApp> {
-  var isCheck = true;
+CupertinoDialogAction buildAlertDialog(BuildContext context) {
+  return CupertinoDialogAction(
+
+    child: Container(
+      height: 300,
+      color: Colors.white,
+      child: Row(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              child: Text("오케잉"),
+              onTap: () {
+                print("오케잉이 클릭 됨");
+              },
+            ),
+          ),
+          Expanded(child: Text("error"))
+              
+        ],
+      ),
+    )
+
+  );
+}
+
+// StatelessWidget 위젯은 한번 그려지면 다시 안그려짐.
+// StatefulWidget 데이터 변경시 다시 그림이 그려짐. Wideget build 함수가 재실행 됨
+class FirstApp extends StatelessWidget {
+  FirstApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-            resizeToAvoidBottomInset: true,
-            body: Stack(
-              children: [
-                Column(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(200),
-                        bottomRight: Radius.circular(200),
-                      ),
-                      child: Container(
-                        alignment: Alignment(0, -0.5),
-                        child: Text(
-                          "GetInThere",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 50,
-                          ),
-                        ),
-                        color: Colors.blue,
-                        height: 400,
-                      ),
-                    ),
-                  ],
-                ),
-                Expanded(child: SizedBox()),
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  height: 200,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Column(
-                      children: [
-                        buildTextFormField("Username"),
-                        buildTextFormField("Password"),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            )),
+      home: Scaffold(
+        body: NewApp(),
       ),
     );
   }
 
-  TextFormField buildTextFormField(String hint) {
-    return TextFormField(
-                      decoration: InputDecoration(
-                        hintText: hint,
-                        enabledBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(width: 1, color: Colors.black),
-                        ),
-                        focusedBorder:OutlineInputBorder(
-                          borderSide: BorderSide(width: 1,color: Colors.black)
-                        ),
-                        fillColor: Colors.grey[300],
-                        filled: true
-                      ),
-                    );
-  }
-
-  ListView buildListView() {
-    return ListView(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(200),
-            bottomRight: Radius.circular(200),
-          ),
-          child: Container(
-            alignment: Alignment(0, -0.5),
-            child: Text(
-              "GetInThere",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 50,
-              ),
-            ),
-            color: Colors.blue,
-            height: 500,
-          ),
-        ),
-        TextFormField(),
-      ],
-    );
-  }
 }
